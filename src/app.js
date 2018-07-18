2804b4cd1f8a4d2f6fb53752b88f9f9b64cf0c9c
Elliot Yandzio <elliotyandzio@gmail.com>
2018-07-18 17:47:49 +0100

    fixed missing babel file which prevented starter code from running

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 class App extends React.Component {
+
   render() {
-    return (
-      <h1>WDI32 Project 4</h1>
-    );
+    return(
+      <h1>Hello, finally got this working</h1>      
+    )
   }
 }
 
